447e60db80e28db6dd387019994c546ebca084e2
Faizan Ejaz <abc@example.com>
2022-05-30 02:32:21 +0500

    Add Functionality Added

--- app/src/main/java/com/example/arraylist_app/MainActivity.java
+++ app/src/main/java/com/example/arraylist_app/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.arraylist_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    Button b1;
+    EditText t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adp =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, grocery);
         lsView.setAdapter(adp);
+
+        b1= findViewById(R.id.button2);
+        t1=findViewById(R.id.editTextTextPersonName2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               grocery.add(t1.getText().toString());
+            }
+        });
     }
 }
