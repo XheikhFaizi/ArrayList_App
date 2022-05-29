456a840793191f716890fb25997b3fbd9c33d459
Faizan Ejaz <abc@example.com>
2022-05-30 01:25:33 +0500

    Apdater Aand Array Added

--- app/src/main/java/com/example/arraylist_app/MainActivity.java
+++ app/src/main/java/com/example/arraylist_app/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.arraylist_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView ls =findViewById(R.id.myListview);
+         ArrayList<String> grocery = new ArrayList<>();
+         grocery.add("Oil");
+         grocery.add("Apple");
+         grocery.add("Pen");
+         grocery.add("Cofffe");
+         grocery.add("Tea");
+
+        ArrayAdapter<String> adp =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, grocery);
+
+
     }
 }
